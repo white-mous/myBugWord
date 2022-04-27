9f3b9887abf95aeff9d52828ed9a99cc5a75e148
whiteMouse <s15113223@stu.edu.tw>
2022-04-27 17:49:00 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 
-# 当发生错误时中止脚本
+# 發生錯誤時執行終止指令
 set -e
 
-# 构建
+# 打包編譯
 npm run build
 
-# cd 到构建输出的目录下
+# 移動到打包資料夾下，若你有調整的話打包後的資料夾請務必調整
 cd dist
 
-# 部署到自定义域域名
+# 部署到自定義網域
 # echo 'www.example.com' > CNAME
 
 git init
@@ -21,5 +21,9 @@ git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# 以這個專案來講就要改成這樣以下這樣，下面是走 ssh 模式
+git push -f git@github.com:hsiangfeng/HexfootMusic.git master:gh-pages
+# 除此之外，也可以改走 HTTPS 模式
+# git push -f https://github.com/hsiangfeng/HexfootMusic.git master:gh-pages
 
 cd -
